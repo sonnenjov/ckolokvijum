c414b0b97af228f6fd17850ad732a9b20db3f63e
Nenad Jovanovic <shone2699@hotmail.rs>
2024-11-29 10:22:07 +0100

    zadatak2

--- labvezbe2/drugi.c
+++ labvezbe2/drugi.c
@@ -2,8 +2,8 @@
 // Napisati program kojim se određuje pozicija cifre od koje će se ponovo naići na početnu kombinaciju 2011.
 
 #include <stdio.h>
-#define MAX 100
-int pravljenjeNiza(int niz[MAX], long n)
+#define MAX 10000
+int pravljenjeNiza(int niz[MAX], long long int n)
 {
   int i = 0;
   while (n>0)
@@ -15,6 +15,22 @@ int pravljenjeNiza(int niz[MAX], long n)
   return i;
 }
 
+
+void prevrtanjeNiza(int niz[MAX], int n)
+{
+  int i,j,pom;
+    for ( i = 0; i < n-1; i++)
+  {
+    for (j = i+1; j < n; j++)
+    {
+      pom=niz[i];
+      niz[i]=niz[j];
+      niz[j]=pom;
+    }
+}
+}
+
+
 void ispisivanjeNiza(int niz[MAX], int broj)
 {
   for (int i = 0; i < broj; i++)
@@ -23,12 +39,31 @@ void ispisivanjeNiza(int niz[MAX], int broj)
   }
 }
 
+void provera(int niz[MAX], int duzina)
+{
+  int i=4;
+  while (1)
+  {
+     niz[i] = (niz[i - 1] + niz[i - 2] + niz[i - 3] + niz[i - 4]) % 10;
+
+     if (niz[i] ==1 && niz[i - 1] == 1 && niz[i - 2] == 0 && niz[i - 3] == 2)
+     {
+      printf("Pozicija od koje pocinje ponavljanje '2011' je: %d\n", i - 3);
+      break;
+     }
+     i++;
+  }
+  
+  
+}
 
 int main()
 {
   int niz[MAX];
-  long broj = 20114623566079;
+  long long int broj = 2011;
   int brojElemenata=pravljenjeNiza(niz,broj);
+  prevrtanjeNiza(niz,brojElemenata);
   ispisivanjeNiza(niz, brojElemenata);
+  provera(niz, brojElemenata);
   return 0;
 }
